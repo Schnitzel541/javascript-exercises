0386f0a2614b168d71f6020ef4c60063e14f312e
Schnitzel541 <j.budding122@gmail.com>
2024-08-06 20:59:04 +0200

    Finished solution 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const { idText } = require("typescript");
+
+const removeFromArray = function(array, ...popIndex) {
+
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!popIndex.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
