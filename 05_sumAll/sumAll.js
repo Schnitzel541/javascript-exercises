fcbb77777d2fdfbdc1d2cd6fc8e40c7cf185900f
Schnitzel541 <j.budding122@gmail.com>
2024-08-06 21:30:47 +0200

    Could only partially solve problem 5 :(

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = "";
 
-};
+    if (firstNum < 0 && lastNum < 0) {
+        return "ERROR";
+    }
+    for (let i = firstNum; i <= lastNum; i++) {
+       
+       if (i < lastNum) {
+        sum += `${i} + `; 
+       }
+       else if (i === lastNum) {
+        sum += `${i}`;
+       }
+       else {
+        break;
+       }
+    }
+    const result = eval(sum);
+    return result;
+}
+    
+
 
 // Do not edit below this line
 module.exports = sumAll;
