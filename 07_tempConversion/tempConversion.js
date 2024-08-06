4ec5b0bb47e0b07a32804b80405233ea4ddb6a3a
Schnitzel541 <j.budding122@gmail.com>
2024-08-06 22:20:31 +0200

    fuck this i quit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let resultInCelsius = (tempInFahrenheit - 30) % 2;
+  let finalResult = Math.round(resultInCelsius * 10) / 10;
+
+  return finalResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let resultInFahrenheit = (tempInCelsius * 2) + 30;
+  let finalResult = Math.round(resultInFahrenheit * 10) / 10;
+
+  return finalResult;
 };
 
 // Do not edit below this line
